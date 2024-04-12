cabf6399ac413807628697f6898b37f201ab1810
Andrey <uniqcle@yandex.ru>
2024-04-12 13:21:57 +0700

    fix/otus debug page

--- OTUS/debug.php
+++ OTUS/debug.php
@@ -4,12 +4,16 @@ $APPLICATION->SetTitle("Новая страница");
 ?>
 <?php
 use Bitrix\Main\Application,
+	Bitrix\Main\Type\DateTime,
     Bitrix\Main\Diag\Debug;
 
 $request = Application::getInstance()->getContext()->getRequest();
 $requestPage = $request->getRequestedPage();
 
-Debug::writeToFile($requestPage, 'Запрошенная страница: ', LOG_FILENAME);
+$currentDateTime = new DateTime();
+$currentDateTimeString = $currentDateTime->toString();
+
+Debug::writeToFile($requestPage, $currentDateTimeString. "\n Запрошенная страница ", LOG_FILENAME);
 
 ?>
 
